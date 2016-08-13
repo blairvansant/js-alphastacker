683778ffb770f1e3bfb14bc089bd037b1b4dba07
blairvansant <blairbot@gmail.com>
2016-08-13 12:41:17 -0500

    finished exercise

--- alpha.js
+++ alpha.js
@@ -16,3 +16,33 @@
 // abcde
 // abcde f
 // etc
+
+var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "x", "y", "z"]
+var newAlpha = []	
+// function alphaStacker(){
+// 	for (var i = 0; i < alphabet.length; i++) {
+// 		alphabet[i] 
+// 		newAlpha += alphabet[i];
+// 		console.log(newAlpha)
+// 	}
+//
+//every fifth letter
+
+var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "x", "y", "z"]
+var newAlpha = []	
+function alphaStacker(){
+	for (var i = 0; i < alphabet.length; i++) {
+		if (i%5 === 0){ 
+			newAlpha += " " + alphabet[i];
+		} else {
+			newAlpha += alphabet[i]
+		}
+		
+		
+		console.log(newAlpha)
+
+
+
+	}
+}
+alphaStacker()
